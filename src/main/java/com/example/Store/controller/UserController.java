d1c298e9ccd81b452ad096a8e9c7b192d648beb4
clair <clairlee07@yahoo.com>
2023-04-27 18:27:06 -0400

    second push

---
+++ src/main/java/com/example/Store/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.example.Store.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/auth/users/login/")
+
+public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    //http://localhost:9094/auth/users/register/
+    @PostMapping(path = "/register/")
+    public SecurityProperties.User createUser(@RequestBody SecurityProperties.User userObject) {
+        return userService.createUser(userObject);
+    }
+
+    //http://localhost:9094/auth/users/login/
+    @PostMapping(path = "/login/")
+    public ResponseEntity<?> loginUser(@RequestBody LoginRequest loginRequest) {
+        return userService.loginUser(loginRequest);
+    }
+
+
+}
