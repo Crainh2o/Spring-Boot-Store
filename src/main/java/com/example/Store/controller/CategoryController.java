3f6ae19be4090ce466b1c004005413744bf17b32
clair <clairlee07@yahoo.com>
2023-04-27 19:37:28 -0400

    push

--- src/main/java/com/example/Store/controller/CategoryController.java
+++ src/main/java/com/example/Store/controller/CategoryController.java
@@ -4,13 +4,13 @@ package com.example.Store.controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/api")
+@RequestMapping(path = "/api") //http://localhost:9094/api/categories/
 public class CategoryController {
     @GetMapping(path = "/api/categories/")
     private String getCategories() {
         return "get all categories";
     }
-
+    //http://localhost:9094/auth/users/login/
     @PostMapping(path = "/auth/users/login/")
     public String createCategory(@RequestBody String body) {
         return "User login  " + body;
--- src/main/java/com/example/Store/controller/UserController.java
+++ src/main/java/com/example/Store/controller/UserController.java
@@ -1,12 +1,8 @@
 package com.example.Store.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/auth/users/login/")
@@ -18,12 +14,6 @@ public class UserController<UserService, LoginRequest> {
     @Autowired
     public void setUserService(UserService userService) {
         this.userService = userService;
-    }
-
-    //http://localhost:9094/auth/users/register/
-    @PostMapping(path = "/register/")
-    public SecurityProperties.User createUser(@RequestBody SecurityProperties.User userObject) {
-        return userService.createUser(userObject);
     }
 
     //http://localhost:9094/auth/users/login/
