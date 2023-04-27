e6329c003b5ed2fa03d682562fa30b50f513b236
clair <clairlee07@yahoo.com>
2023-04-27 17:32:21 -0400

    first push

---
+++ src/main/java/com/example/Store/controller/CategoryController.java
@@ -0,0 +1,18 @@
+package com.example.Store.controller;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/api")
+public class CategoryController {
+    @GetMapping(path = "/api/categories/")
+    private String getCategories() {
+        return "get all categories";
+    }
+
+    @PostMapping(path = "/auth/users/login/")
+    public String createCategory(@RequestBody String body) {
+        return "User login  " + body;
+    }
+}
