4c51ccd1e761eb98aab670426494224530e3eb84
clair <clairlee07@yahoo.com>
2023-04-28 12:43:29 -0400

    push

--- src/main/java/com/example/Store/service/ProductService.java
+++ src/main/java/com/example/Store/service/ProductService.java
@@ -1,13 +1,23 @@
 package com.example.Store.service;
 
+import com.example.Store.repository.OrdersRepository;
 import com.example.Store.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ProductService {
+    private OrdersRepository ordersRepository;
+    private ProductRepository productRepository;
 
-    private final ProductRepository productRepository;
-    private ProductService productService;
-    
-    public ProductService(ProductRepository productRepository) {
+    @Autowired
+    public void setOrdersRepository(OrdersRepository ordersRepository) {
+        this.ordersRepository = ordersRepository;
+    }
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
         this.productRepository = productRepository;
     }
+
+
 }
