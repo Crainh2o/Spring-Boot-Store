3b55c87f5d21728e97e25441708909feb6a98cd9
clair <clairlee07@yahoo.com>
2023-04-28 11:52:05 -0400

    push

--- src/main/java/com/example/Store/controller/ProductController.java
+++ src/main/java/com/example/Store/controller/ProductController.java
@@ -4,11 +4,11 @@ package com.example.Store.controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(path = "/api") //http://localhost:9094/api/categories/
+@RequestMapping(path = "/api") //http://localhost:9094/api/products/
 public class ProductController {
-    @GetMapping(path = "/api/categories/")
-    private String getCategories() {
-        return "get all categories";
+    @GetMapping(path = "/api/products/")
+    private String getProducts() {
+        return "get all products";
     }
 
 }
