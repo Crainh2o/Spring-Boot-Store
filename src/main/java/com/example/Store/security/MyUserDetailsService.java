9323d6106b6a513070106b4c16ab3d7ed94299ea
clair <clairlee07@yahoo.com>
2023-04-28 20:38:33 -0400

    push

--- src/main/java/com/example/Store/security/MyUserDetailsService.java
+++ src/main/java/com/example/Store/security/MyUserDetailsService.java
@@ -1,4 +1,28 @@
 package com.example.Store.security;
 
-public class MyUserDetailsService {
+import com.example.Store.model.User;
+import com.example.Store.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findUserByEmailAddress(email);
+        return new MyUserDetails(user);
+    }
 }
+
+
+
